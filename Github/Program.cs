21cc033f2feec4ee896a911e25594551a00f40ab
Szabolcs01 <oszabi000@gmail.com>
2020-09-27 11:25:35 +0200

    Felhasznákó létre hozzása es 10 szam be kérése és átlag számolás

--- Github/Program.cs
+++ Github/Program.cs
@@ -10,6 +10,15 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            int[] szamok = new int[10];
+            Console.WriteLine("Kérem adjon meg 10 számot");
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("{0}.szam:",i+1);
+                szamok[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("A be kért szamok átlaga:{0}",szamok.Average());
+            Console.ReadKey();
         }
     }
 }
